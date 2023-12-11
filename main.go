2b5e38d7736cbc98c94c2f2db82c6d4b44223598
Gustavo <gustanley@gmail.com>
2023-12-11 18:44:04 -0300

    add chi router

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	fmt.Println("Server running on port 3000")
+	http.ListenAndServe(":3000", r)
 }
